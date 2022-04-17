36ca1c4a1cd66252479a482b67de963389c58a7a
Proton2-Limitless <69195632+Proton2-Limitless@users.noreply.github.com>
2022-04-17 21:01:54 +0100

    putchar file

--- _putchar.c
+++ _putchar.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(int c)
+int _putchar(char c)
 {
-	static char buf[1024];
-	static int pos;
-
-	if (c == -1 || pos >= 1024)
-	{
-		write(1, buf, pos);
-		pos = 0;
-	}
-	if (c != -1)
-	{
-		buf[pos++] = c;
-	}
-	return (1);
+	return (write(1, &c, 1));
 }
