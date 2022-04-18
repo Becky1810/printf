8cf81ab3c8deea5fac47e81864b94b22064b6ad6
Becky1810 <berikisuharuna456@gmail.com>
2022-04-18 12:07:28 +0100

    a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -22,5 +22,29 @@ int _printf(const char *format, ...)
 			return (-1);
 
 		count = 0;
-    }
+	
+	        for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					count += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+				count += _putchar(format[i]);
+		}
+		va_end(ar_list);
+	}
+
+	return (count);
 }
+   
